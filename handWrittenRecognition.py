d5b644d54c28b7059bfb8751646bb804b4522b3b
cclp94 <cclp94@gmail.com>
2017-03-16 20:13:52 -0400

    Normalized and refactored training data

--- handWrittenRecognition.py
+++ handWrittenRecognition.py
@@ -9,7 +9,11 @@ trainingList = []
 validationList = []
 testingList = []
 
+def normalizeFeatures(feature):
+    return 0 if feature < 50 else 1
+
 def readData(fileName):
+    global trainingList, validationList, testingList
     img_file = open(fileName,'r+b')
     # Go to beginning of file
     img_file.seek(0)
@@ -22,9 +26,9 @@ def readData(fileName):
     numImages = struct.unpack('>i',numImages)[0]
 
     #calculate size of training, validation and testing sets
-    numTraining = round((numImages * (TRAINING_PERCENTAGE / 100.0)), 0)
-    numValidation = round((numImages * (VALIDATION_PERCENTAGE / 100.0)), 0)
-    numTesting = round((numImages * (TESTING_PERCENTAGE / 100.0)), 0)
+    numTraining = int(round((numImages * (TRAINING_PERCENTAGE / 100.0)), 0))
+    numValidation = int(round((numImages * (VALIDATION_PERCENTAGE / 100.0)), 0))
+    numTesting = int(round((numImages * (TESTING_PERCENTAGE / 100.0)), 0))
 
     print(str(numTraining), str(numValidation), str(numTesting), str(numTraining+numValidation+numTesting))
 
@@ -36,18 +40,26 @@ def readData(fileName):
     columns = img_file.read(4)
     columns = struct.unpack('>i',columns)[0]
 
-    # Get each image
-    for i in range(numImages) :
-        img = img_file.read(rows * columns)
-        img = list(img)
-        if(i < numTraining):    # Put into training set
-            trainingList.append(img)
-        elif i < numTraining + numValidation:   # Put into validation set
-            validationList.append(img)
-        else:                   # Put into testing set
-            testingList.append(img)
+    # Get Training portion
+    trainingList = img_file.read(rows * columns * numTraining)
+    trainingList = list(map(normalizeFeatures, trainingList))   # Normalize
 
+    trainingList = [trainingList[i:i + (rows*columns)] for i in range(0, len(trainingList), (rows*columns))]
+
+    # Get Validation portion
+    validationList = img_file.read(rows * columns * numValidation)
+    validationList = list(map(normalizeFeatures, validationList))   # Normalize
+
+    validationList = [validationList[i:i + (rows*columns)] for i in range(0, len(validationList), (rows*columns))]
+
+    # Get Testing portion
+    testingList = img_file.read(rows * columns * numTesting)
+    testingList = list(map(normalizeFeatures, testingList))     # Normalize
+
+    testingList = [testingList[i:i + (rows*columns)] for i in range(0, len(testingList), (rows*columns))]
+
+    #close file
     img_file.close()
 
 if __name__ == '__main__':
-	readData('data/mnist-data')
+	readData('data/mnist-train')
